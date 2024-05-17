b225a46e6604cbab8c90ba6c7d96f4ee3800e028
hey-dudegg <m5rep5wer>
2024-05-17 22:30:59 +0900

    DJ2

--- frontend/src/components/Problem/ChatComponents/CreateChatSocket.tsx
+++ frontend/src/components/Problem/ChatComponents/CreateChatSocket.tsx
@@ -1,14 +1,17 @@
 import { io } from 'socket.io-client';
 type SocketCallback = Record<string, any>;
 
-export default function createSocket(socketURL: string, socketCallback: SocketCallback) {
+export default function createSocket(
+  socketURL: string,
+  socketCallback: SocketCallback,
+) {
   const socket = io(socketURL, {
     transports: ['websocket'],
     reconnection: true,
     reconnectionAttempts: 5,
     reconnectionDelay: 1000,
   });
-  
+
   Object.entries(socketCallback).forEach(([event, callback]) => {
     socket.on(event, callback);
   });

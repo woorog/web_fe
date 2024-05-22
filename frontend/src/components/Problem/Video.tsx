54f6a40563c87dfe89806e264c0898b79c7adde8
DOHA <koo9811@gmail.com>
2024-05-22 22:00:25 +0900

    video edit

--- frontend/src/components/Problem/Video.tsx
+++ frontend/src/components/Problem/Video.tsx
@@ -37,30 +37,29 @@ export const Video = () => {
   const [, setMyID] = useState('');
   const [peers, setPeers] = useState<{ [id: string]: MediaStream }>({});
   const [videoOn, setVideoOn] = useState(false);
-  const [micOn, setMicOn] = useState(false);
   const [text, setText] = useState('');
   const [btnWork, setBtnWork] = useState(false);
+  const [recordedBlobSize, setRecordedBlobSize] = useState(0);
   const peerVideosRef = useRef<Array<HTMLVideoElement>>([]);
   const navigate = useNavigate();
-  const [isSpeaking, setIsSpeaking] = useState(false);
 
   const [myPeer, setMyPeer] = useState<Peer>();
   const [socket, setSocket] = useRecoilState(socketState);
+  const lastLogTimeRef = useRef(Date.now());
 
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({ video: true, audio: true })
       .then((mediaStream) => {
         console.log('getUserMedia success:', mediaStream);
         setVideoOn(true);
-        setMicOn(true);
         setMyStream(mediaStream);
         setMyPeer(new Peer());
 
         const socketUrl = import.meta.env.VITE_SOCKET_SERVER_URL;
 
         const newSocket = io(socketUrl, {
-          // path: '/socket-video/',
-          // secure: true,
+          path: '/socket-video/',
+          secure: true,
           reconnection: true,
           reconnectionAttempts: 5,
           reconnectionDelay: 1000,
@@ -75,20 +74,25 @@ export const Video = () => {
   }, []);
 
   const handleMicData = (recordedBlob: any) => {
-    console.log('recordedBlob is: ', recordedBlob);
+    const currentTime = Date.now();
     if (recordedBlob && recordedBlob.size > 0) {
-      console.log('recordedBlob size: ', recordedBlob.size);
-      if (micOn) {
-        setIsSpeaking(true);
+      setRecordedBlobSize(recordedBlob.size);
+      if (currentTime - lastLogTimeRef.current >= 10000) { // 10초마다 로그 출력
+        console.log('recordedBlob size: ', recordedBlob.size);
+        lastLogTimeRef.current = currentTime; // 마지막 로그 출력 시간 업데이트
       }
     } else {
+      setRecordedBlobSize(0);
       console.log('recordedBlob size is 0 or undefined');
-      setIsSpeaking(false);
+      if (currentTime - lastLogTimeRef.current >= 10000) { // 10초마다 로그 출력
+        lastLogTimeRef.current = currentTime; // 마지막 로그 출력 시간 업데이트
+      }
     }
   };
 
   const handleMicStop = (recordedBlob: any) => {
     console.log('Recorded Blob:', recordedBlob);
+    setRecordedBlobSize(0);
   };
 
   const callCallback = useCallback(
@@ -254,7 +258,7 @@ export const Video = () => {
     setBtnWork(true);
     const updateConstraints: ConstraintsType = {};
     updateConstraints.video = !videoOn ? videoSize : false;
-    updateConstraints.audio = micOn;
+    updateConstraints.audio = recordedBlobSize > 0;
     setTimeoutText(`카메라 ${!videoOn ? 'ON' : 'OFF'}`);
     setVideoOn(!videoOn);
     sendStream(updateConstraints);
@@ -268,19 +272,27 @@ export const Video = () => {
     setBtnWork(true);
     const updateConstraints: ConstraintsType = {};
     updateConstraints.video = videoOn ? videoSize : false;
-    updateConstraints.audio = !micOn;
-    setTimeoutText(`마이크 ${!micOn ? 'ON' : 'OFF'}`);
-    setMicOn(!micOn);
-    if (!micOn) {
-      setIsSpeaking(false); // 마이크를 끌 때 isSpeaking을 false로 설정
-    }
+    const newMicOnStatus = recordedBlobSize === 0;
+    updateConstraints.audio = newMicOnStatus;
+    setTimeoutText(`마이크 ${newMicOnStatus ? 'ON' : 'OFF'}`);
+    setRecordedBlobSize(newMicOnStatus ? 1000 : 0); // 마이크 끌 때 recordedBlob을 0으로 설정
+    console.log('Mic button clicked. New Mic status:', newMicOnStatus);
     sendStream(updateConstraints);
   };
 
+  useEffect(() => {
+    if (recordedBlobSize > 1000) {
+      const timer = setTimeout(() => {
+        setRecordedBlobSize(1000); // 1.5초 후에 recordedBlobSize를 1000으로 설정
+      }, 1500);
+      return () => clearTimeout(timer); // 타이머를 정리하여 메모리 누수를 방지
+    }
+  }, [recordedBlobSize]);
+  
   return (
     <div className="mt-4 w-full min-h-36 flex flex-col justify-start items-center overflow-auto">
       <ReactMic
-        record={micOn}
+        record={recordedBlobSize > 0}
         className="hidden"
         onStop={handleMicStop}
         onData={handleMicData}
@@ -288,32 +300,52 @@ export const Video = () => {
         backgroundColor="#FF4081"
       />
       {Object.entries(peers).map((user, idx) => (
-        <video
-          className={`max-w-48 w-full max-h-36 h-auto mb-4 bg-white rounded-lg ${isSpeaking ? 'border-4 border-green-500' : ''}`}
-          autoPlay
-          playsInline
-          ref={(ele) => {
-            if (ele) {
-              peerVideosRef.current[idx] = ele;
-            }
-          }}
+        <div
           key={idx}
-        />
+          className="relative max-w-48 w-full max-h-36 h-auto mb-4"
+        >
+          <div
+            className={`absolute inset-0 rounded-lg ${recordedBlobSize > 1000 ? 'border-4 border-green-500' : 'bg-transparent'}`}
+            style={{
+              boxShadow: recordedBlobSize > 1000 ? '0 0 15px 5px rgba(0, 255, 0, 0.7)' : 'none',
+              pointerEvents: 'none' // This ensures that the div does not interfere with video interactions
+            }}
+          ></div>
+          <video
+            className="relative w-full h-full object-cover rounded-lg"
+            autoPlay
+            playsInline
+            ref={(ele) => {
+              if (ele) {
+                peerVideosRef.current[idx] = ele;
+              }
+            }}
+          />
+        </div>
       ))}
       <div className="relative max-h-36 w-full h-auto flex flex-col items-center">
-        <video
-          className={`max-w-48 w-full max-h-36 h-auto mb-4 bg-white rounded-lg ${isSpeaking ? 'border-4 border-green-500' : ''}`}
-          ref={videoRef}
-          autoPlay
-          muted
-          playsInline
-        />
+        <div className="relative max-w-48 w-full max-h-36 h-auto mb-4">
+          <div
+            className={`absolute inset-0 rounded-lg ${recordedBlobSize > 1000 ? 'border-4 border-green-500' : 'bg-transparent'}`}
+            style={{
+              boxShadow: recordedBlobSize > 1000 ? '0 0 15px 5px rgba(0, 255, 0, 0.7)' : 'none',
+              pointerEvents: 'none' // This ensures that the div does not interfere with video interactions
+            }}
+          ></div>
+          <video
+            className="relative w-full h-full object-cover rounded-lg"
+            ref={videoRef}
+            autoPlay
+            muted
+            playsInline
+          />
+        </div>
         <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 flex gap-2">
           <div className="cursor-pointer text-base text-center z-10" onClick={handleMicButton}>
-            {micOn ? <Volume className="w-5 h-5"/> : <VolumeSlash className="w-5 h-5"/>}
+            {recordedBlobSize > 0 ? <Volume className="w-5 h-5" /> : <VolumeSlash className="w-5 h-5" />}
           </div>
           <div className="cursor-pointer text-base text-center z-10" onClick={handleCameraButton}>
-            {!videoOn ? <Camera className="w-5 h-5"/> : <Pause className="w-5 h-5"/>}
+            {!videoOn ? <Camera className="w-5 h-5" /> : <Pause className="w-5 h-5" />}
           </div>
         </div>
         <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-sm text-gray-600 text-center w-full">
@@ -321,5 +353,5 @@ export const Video = () => {
         </div>
       </div>
     </div>
-  );
+  );  
 };
